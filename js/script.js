e2989ffa5b4689e9d9bb424613685d0acdcbe008
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-09-01 23:47:44 +0600

    designing card

--- js/script.js
+++ js/script.js
@@ -6,17 +6,34 @@ const handleCategories = async () => {
 
   const categoriesContainer = document.getElementById("categories-container");
 
-  data.data.forEach((category) => {
+  data.data?.forEach((category) => {
     const button = document.createElement("button");
     button.innerHTML = `
     <button
-          class="text-[#252525b3] text-xs sm:text-base font-medium py-2 px-5 bg-[#25252526] rounded mx-[6px] sm:mx-3 mb-3"
+          onclick=handleLoadCards('${category.category_id}') class="text-[#252525b3] text-xs sm:text-base font-medium py-2 px-5 bg-[#25252526] rounded mx-[6px] sm:mx-3 mb-3"
         >
           ${category.category}
     </button>
     `;
 
     categoriesContainer.appendChild(button);
+  });
+};
+
+const handleLoadCards = async (categoryId) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
+  );
+  const data = await res.json();
+
+  const cardsContainer = document.getElementById("cards-container");
+
+  data.data?.forEach((card) => {
+    const singleCard = document.createElement("div");
+    singleCard.innerHTML = `
+    
+    `;
+    cardsContainer.appendChild(singleCard);
   });
 };
 
